5b0f3d2b1daa19bb3bb2d9d0ddeb73a257237a17
Tomaz Zazijal <tomaz.zazijal@gmail.com>
2023-08-28 23:27:03 +0200

    Added: - endpoint for fetching temperature readings for the past 24 hours

---
+++ iot-temp-fn-app/temperature_last_24hours.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace iot_temp_fn_app
+{
+    public static class temperature_last_24hours
+    {
+        [FunctionName("temperature_last_24hours")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+           [CosmosDB(
+                databaseName:"temptelemetry",
+                containerName:"temptelemetry",
+                Connection = "CosmosDbConnectionString",
+                SqlQuery = "SELECT * FROM temptelemetry t WHERE timestampToDateTime(t._ts*1000) >= dateTimeAdd(\"dd\", -1, getCurrentDateTime()) ORDER BY t._ts DESC"
+            )]IEnumerable<TemperatureTelemetry> tempModels,
+           ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            List<TemperatureTelemetryModel> ttm = new List<TemperatureTelemetryModel>();
+
+            foreach (TemperatureTelemetry t in tempModels)
+            {
+                TemperatureTelemetryModel tm = new TemperatureTelemetryModel()
+                {
+                    Id = t.Id,
+                    PartitionKey = t.PartitionKey,
+                };
+
+                byte[] data = Convert.FromBase64String(t.Body);
+                tm.Temperature = System.Text.Encoding.UTF8.GetString(data);
+
+                tm.Timestamp = DateTimeOffset.FromUnixTimeSeconds(t.Timestamp).UtcDateTime;
+
+                ttm.Add(tm);
+            }
+
+            return new OkObjectResult(JsonConvert.SerializeObject(ttm));
+        }
+    }
+}
