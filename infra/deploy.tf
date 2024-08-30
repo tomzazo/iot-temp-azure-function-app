c6e3a6e94e068bcc7529987a9bce1048b54727fd
Tomaz Zazijal <tomaz.zazijal@gmail.com>
2024-08-30 17:42:28 +0200

    chore(infra): update to dotnet8

--- infra/deploy.tf
+++ infra/deploy.tf
@@ -34,7 +34,7 @@ resource "azurerm_linux_function_app" "iot-temp-fn-app" {
 
   site_config {
     application_stack {
-      dotnet_version = "6.0"
+      dotnet_version = "8.0"
       use_dotnet_isolated_runtime = "false"
     }
   }
